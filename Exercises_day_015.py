2c6c5012661ffc18ae5d77701507ca58b1354694
Hassan Adamu <124632645+abufaiz@users.noreply.github.com>
2023-02-10 23:37:07 +0100

    Add files via upload

---
+++ Exercises_day_015.py
@@ -0,0 +1,21 @@
+# Day 15 - 30DaysOfPython Exercises 
+
+print(''' After running all the examples in day 15 in the python shell, I realised
+      that the following were the errors 
+
+1) SyntaxError
+2) NameError
+3) IndexError
+4) ModuleNotFoundError
+5) AttributeError
+6) KeyError
+7) TypeError
+8) ImportError
+9) ValueError
+10) ZeroDivisionError
+
+Even though some of the errors were corrected by the python as suggestion, 
+i have learnt alot in tackling errors whenever I identify any
+
+ 
+''')
